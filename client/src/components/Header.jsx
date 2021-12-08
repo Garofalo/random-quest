8e6cb02ffac19cacf29804b2972148275da9ac53
Rgarf <nothanks@gmail.com>
2021-12-08 18:02:23 -0500

    touch up

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header>
       
-      <Link to='/'><img src={logoThin} id='head-logo'/></Link>
+      <Link to='/'><img src={logoThin} id='head-logo' alt='random quest'/></Link>
     </header>
   )
 }
